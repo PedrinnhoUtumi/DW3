dc2945a8ebaf86925f9d2e00c5f680c228b9d9f5
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2025-04-09 11:52:24 -0300

    .

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const app = express()
+const porta = 8806
+
+
+app.get('/', (req, res) => {
+    res.send('Olá Mundo')
+})
+
+app.get('/isbn/:isbn', (req, res) => {
+    fetch(`https://brasilapi.com.br/api/isbn/v1/${req.params.isbn}`)
+        .then((response) => response.json())
+        .then((nome) => {
+            const author = Array.isArray(nome.authors) ? nome.authors.map(author => `<li>${author}</li>`).join('') : "Autor não encontrado";
+            const subject = Array.isArray(nome.subjects) ? nome.subjects.map(subject => `<li>${subject}</li>`).join('') : "Tema não encontrado";
+
+            res.send(`
+                <!DOCTYPE html>
+                <html lang="pt-BR">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Informações do ISBN</title>
+                </head>
+                <body>
+                    <h1>Detalhes do Livro</h1>
+                    <p><strong>Nome:</strong> ${nome.title}</p>
+                    <p><strong>Autor:</strong> ${author}</p>
+                    <p><strong>Tema:</strong> ${subject}</p>
+                </body>
+                </html>
+                `)
+        })
+        .catch(error => {
+            res.send("Algo deu erro")
+        })
+})
+
+app.get('/ddd/:ddd', (req, res) => {
+    fetch(`https://brasilapi.com.br/api/ddd/v1/${req.params.ddd}`)
+        .then((response) => response.json())
+        .then((cidade) => {
+            const cities = Array.isArray(cidade.cities) ? cidade.cities.map(city => `<li>${city}</li>`).join('') : "Cidades não encontradas";
+
+            res.send(`
+                <!DOCTYPE html>
+                <html lang="pt-BR">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Informações do ISBN</title>
+                </head>
+                <body>
+                    <h1>Estados e cidades do DDD</h1>
+                    <p><strong>Estado:</strong> ${cidade.state}</p>
+                    <p><strong>Cidades:</strong> ${cities}</p>
+                </body>
+                </html>
+                `)
+        })
+        .catch(error => {
+            res.send("Algo deu erro")
+        })
+})
+
+app.listen(porta, () => {
+    console.log(`servidor rodando na porta ${porta}`);    
+})
